1f387f7420edcf46367d0c937a88c0d7fe7e9bbc
ChemaSalazar <ChemaSalazar@Outlook.com>
2021-06-06 16:59:38 -0500

    Added Categories and Category views.

--- Landmarks/Models/ModelData.swift
+++ Landmarks/Models/ModelData.swift
@@ -13,6 +13,18 @@ import Combine
 final class ModelData: ObservableObject {
     //this is the file subscriber's get new data changes from
     @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
+    
+    var features: [Landmark]{
+        landmarks.filter{ $0.isFeatured}
+    }
+    
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks, by: {$0.category.rawValue}
+        )
+    }
 }
 
 
